5faabfc4002c70e1b7e85c28e1640513a8ecccf7
Diego Arevalo Fernandez <darevalofernandez@ucsd.edu>
2025-02-01 21:09:03 -0800

    alright good, keep working

---
+++ index.js
@@ -0,0 +1,14 @@
+import { fetchJSON, renderProjects, fetchGithubData} from './global.js';
+
+async function loadLatestProjects() {
+
+    const projects = await fetchJSON('./lib/projects.json');
+  
+    const latestProjects = projects.slice(0, 3);
+    
+    const projectsContainer = document.querySelector('.projects');
+    
+    renderProjects(latestProjects, projectsContainer, 'h2');
+  }
+  
+  loadLatestProjects();
