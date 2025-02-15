7aa7d4cc8e32d88bc8671eb46129c0994dc383e3
Diego Arevalo Fernandez <darevalofernandez@ucsd.edu>
2025-02-14 21:57:35 -0800

    alright

--- meta/main.js
+++ meta/main.js
@@ -3,19 +3,24 @@ let data = [];
 async function loadData() {
     data = await d3.csv('loc.csv', (row) => ({
         ...row,
-        line: Number(row.line), // or just +row.line
+        line: Number(row.line),
         depth: Number(row.depth),
         length: Number(row.length),
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
 
-    // Once data is loaded, call processCommits() and displayStats()
+    processCommits();
     displayStats();
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadData();
+
+
+    console.log('Commits:', commits);
+
+    await createScatterplot();
 });
 
 let commits = [];
@@ -50,18 +55,18 @@ function processCommits() {
 }
 
 function displayStats() {
-    // Create the dl element in the main element
+
     const dl = d3.select('main').append('dl').attr('class', 'stats');
 
-    // Add total LOC
+    
     dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
     dl.append('dd').text(data.length);
 
-    // Add total commits
+   
     dl.append('dt').text('Total commits');
     dl.append('dd').text(commits.length);
 
-    // Add more stats as needed...
+    
     const numberOfFiles = d3.group(data, (d) => d.file).size;
     dl.append('dt').text('Number of files');
     dl.append('dd').text(numberOfFiles);
@@ -73,4 +78,76 @@ function displayStats() {
     const longestFile = d3.max(data, (d) => d.file);
     dl.append('dt').text('Longest file');
     dl.append('dd').text(longestFile);
+}
+
+function createScatterplot() {
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
+    
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+      };
+      
+      // Update scales with new ranges
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
+
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+// Create gridlines as an axis with no labels and full-width ticks
+gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add X axis
+    svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    // Add Y axis
+    svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+
 }
